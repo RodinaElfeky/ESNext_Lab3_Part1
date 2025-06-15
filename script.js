526556500763618a4f4e51497c9db5c521678726
Rodina ElFeky <rodina.elfeky228@gmail.com>
2025-06-15 16:28:05 +0300

    updates

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+var food = ['Burger', 'Pizza', 'Donuts', 'Pizza', 'Koshary', 'Donuts', 'Seafood','Burger']
 
+//------1)
+var foodSet = new Set(food)
+
+//------2)
+foodSet.add('Pasta')
+console.log(foodSet)
+
+//------3)
+foodSet.delete('Burger')
+console.log(foodSet)
+
+//------4)
+function clear(Set) {
+    if (Set.size > 2) {
+        Set.clear()
+    } 
+}
+
+clear(foodSet)
+
+console.log(foodSet)
